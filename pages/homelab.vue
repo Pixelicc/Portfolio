9eee72250c1bd72b5dfb18222513413d476894c5
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2025-01-15 18:15:40 +0100

    Update homelab.vue

--- pages/homelab.vue
+++ pages/homelab.vue
@@ -99,10 +99,10 @@
 <script setup lang="ts">
 import type { HomelabSpecs, HomelabMetrics } from "~/types";
 
-const { data: specs } = useFetch<{ specs: HomelabSpecs }>(
+const specs = useFetch<{ specs: HomelabSpecs }>(
   "https://api.pixelic.dev/v2/homelab/specs",
 );
-const { data: metrics } = useFetch<{ metrics: HomelabMetrics }>(
+const metrics = useFetch<{ metrics: HomelabMetrics }>(
   "https://api.pixelic.dev/v2/homelab/metrics",
 );
 </script>
