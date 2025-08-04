27601243a21652da5f63fee3dda2af782916b60e
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2025-08-04 12:22:27 +0200

    Bumped Dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-03-01",
-  future: {
-    compatibilityVersion: 4,
-  },
   experimental: {
     inlineRouteRules: true,
   },
